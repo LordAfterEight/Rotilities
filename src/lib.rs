fc0d72d9b3df461f55d96754db1d620af8a856f5
LordAfterEight <elias@stettmayer.de>
2025-05-28 10:06:15 +0200

    v0.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,11 @@
 
 
 use std::{fs::File, io::BufReader};
+#[cfg(not(target_arch = "aarch64"))]
 use rodio::Decoder;
 
 ///Internal helper function for this crate
+#[cfg(not(target_arch = "aarch64"))]
 pub fn get_source(filename: &str) -> Decoder<BufReader<File>> {
     let source = rodio::Decoder::new(
         BufReader::new(
@@ -34,6 +36,7 @@ pub fn get_source(filename: &str) -> Decoder<BufReader<File>> {
 }
 
 ///This function "initiates" the crucial part of audio output, the stream and stream handle, and is thus always required
+#[cfg(not(target_arch = "aarch64"))]
 pub fn init() -> (rodio::OutputStream, rodio::OutputStreamHandle) {
     return rodio::OutputStream::try_default()
         .expect("Creating OuputStream Failed")
@@ -48,6 +51,7 @@ pub fn init() -> (rodio::OutputStream, rodio::OutputStreamHandle) {
 /// let (_stream, stream_handle) = init()
 /// let sink = new_sink(&stream_handle);
 /// ```
+#[cfg(not(target_arch = "aarch64"))]
 pub fn new_sink(stream_handle: &rodio::OutputStreamHandle) -> rodio::Sink {
     return rodio::Sink::try_new(stream_handle).expect("Sink creation failed")
 }
@@ -64,6 +68,7 @@ pub fn new_sink(stream_handle: &rodio::OutputStreamHandle) -> rodio::Sink {
 /// let sink = new_sink();
 /// play_audio(&sink, "path/to/file.mp3");
 /// ```
+#[cfg(not(target_arch = "aarch64"))]
 pub fn play_audio(sink: &rodio::Sink, filename: &str) {
     sink.append(get_source(&format!("{filename}")));
 }
@@ -81,6 +86,7 @@ pub fn play_audio(sink: &rodio::Sink, filename: &str) {
 ///
 /// stop_audio(&sink);
 /// ```
+#[cfg(not(target_arch = "aarch64"))]
 pub fn stop_audio(sink: &rodio::Sink) {
     sink.stop();
 }
@@ -98,6 +104,7 @@ pub fn stop_audio(sink: &rodio::Sink) {
 ///
 /// pause_audio(&sink);
 /// ```
+#[cfg(not(target_arch = "aarch64"))]
 pub fn pause_audio(sink: &rodio::Sink) {
     sink.pause();
 }
@@ -118,6 +125,7 @@ pub fn pause_audio(sink: &rodio::Sink) {
 ///
 /// resume_audio(&sink);
 /// ```
+#[cfg(not(target_arch = "aarch64"))]
 pub fn resume_audio(sink: &rodio::Sink) {
     sink.play();
 }
@@ -135,6 +143,31 @@ pub fn resume_audio(sink: &rodio::Sink) {
 ///
 /// set_audio_volume(&sink, 0.5);
 /// ```
+#[cfg(not(target_arch = "aarch64"))]
 pub fn set_audio_volume(sink: &rodio::Sink, volume: f32) {
     sink.set_volume(volume);
 }
+
+#[cfg(target_arch = "aarch64")]
+pub fn init() -> (String,String) {
+    let string = String::new();
+    return (string.clone(),string)
+}
+
+#[cfg(target_arch = "aarch64")]
+pub fn new_sink(_p: String) {}
+
+#[cfg(target_arch = "aarch64")]
+pub fn play_audio(_p: String) {}
+
+#[cfg(target_arch = "aarch64")]
+pub fn stop_audio(_p: String) {}
+
+#[cfg(target_arch = "aarch64")]
+pub fn pause_audio(_p: String) {}
+
+#[cfg(target_arch = "aarch64")]
+pub fn resume_audio(_p: String) {}
+
+#[cfg(target_arch = "aarch64")]
+pub fn set_audio_volume(_p: String,_p2: f32) {}
